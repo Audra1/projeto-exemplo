243ba0c468e50117e7c98ce97d497a45c202253c
Audra1 <lucasaudra1@gmail.com>
2024-12-02 08:43:15 -0300

    commits

--- editora/settings.py
+++ editora/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'editora.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
+    'postgresql://exemplo_livro_5js7_user:6SLfvGg7okhzoHTAT9anCPdVoPLOXmuv@dpg-ct4d0rl6l47c73f8mce0-a.oregon-postgres.render.com/exemplo_livro_5js7': dj_database_url.config(
         default='',
         conn_max_age=600,
         ssl_require=not DEBUG
